520d13cca21479f17e0523638abe1ca6bec5919b
RuthwikHM <41834689+RuthwikHM@users.noreply.github.com>
2019-11-17 11:49:58 +0530

    Final frontend upload.

--- js/script.js
+++
@@ -1,8 +0,0 @@
-function login(usr,pw) {
-    let uname = "Ruthwik";
-    let pwd = "00000";
-    if(usr === uname && pw === pwd)
-        window.location = "../html/Aboutus.html";
-    else
-        alert("Invalid username or password.");
-}
